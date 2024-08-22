ccce0d98661444e4c8869b7ed08660f88e7797bf
GonGarciaFontenla <ggarciafontenla@frba.utn.edu.ar>
2024-08-21 23:27:37 -0300

    Fisher calculator

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Aquí puedes agregar la lógica para manejar los clics en los enlaces del menú y mostrar el contenido correspondiente.
-document.querySelectorAll('nav ul li a').forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        alert('Esta funcionalidad se implementará próximamente.');
-    });
-});
+// // Aquí puedes agregar la lógica para manejar los clics en los enlaces del menú y mostrar el contenido correspondiente.
+// document.querySelectorAll('nav ul li a').forEach(link => {
+//     link.addEventListener('click', function(event) {
+//         event.preventDefault();
+//         alert('Esta funcionalidad se implementará próximamente.');
+//     });
+// });
 
 // Mostrar el formulario de Fisher cuando se hace clic en el enlace
 document.getElementById('menu-fisher').addEventListener('click', function(event) {
